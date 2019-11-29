08bb61673dba9e063ee7618723e6971008840fcd
RizkyAlmsyah <rizkyalamsyah@it.student.pens.ac.id>
2019-11-29 18:34:53 +0700

    update: CarController & CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -29,4 +29,127 @@ class CategoryController extends Controller
             }
         }
     }
+
+    public function show($id){    
+        try {
+            $jsonObjs = json_decode($this->getCategory($id));
+
+            return view('data.detail-category', ['category' => $jsonObjs]);
+        } catch(\GuzzleHttp\Exception\BadResponseException $e) {
+            if($e->getResponse()->getStatusCode() == 401) {
+                return redirect()
+                    ->route('login');
+            } else {
+                echo($e->getResponse()->getBody());
+            }
+        }
+    }
+
+    public function getCategory($id)
+    {
+        $token = Session::get('token');
+        return $response= $this->client->request('GET', $this->base_url.'/category/'.$id, [
+            'headers' => [
+                'Authorization' => "Bearer {$token}"
+                ]
+        ])->getBody()->getContents();
+    }
+
+    public function delete($id)
+    {
+        try {
+            $token = Session::get('token');
+            $response= $this->client->request('DELETE', $this->base_url.'/category/'.$id.'/delete', [
+                'headers' => [
+                    'Authorization' => "Bearer {$token}"
+                    ]
+            ])->getBody()->getContents();
+
+            return redirect()
+                ->route('category')
+                ->with('success', 'Category has been deleted!');
+        } catch(\GuzzleHttp\Exception\BadResponseException $e) {
+            if($e->getResponse()->getStatusCode() == 401) {
+                return redirect()
+                    ->route('login');
+            } else {
+                echo($e->getResponse()->getBody());
+            }
+        }
+    }
+    
+    public function create()
+    {
+        if(Session::get('token')) {
+            return view('data.createOrUpdate-category');
+        } else {
+            return redirect()
+                ->route('login');
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            $token = Session::get('token');
+            $response = $this->client
+                ->request('POST', $this->base_url.'/category',  [
+                    'form_params' => [
+                        'category_name' => $request->input('category_name'),
+                    ],
+                    'headers' => [
+                        'Authorization' => "Bearer {$token}"
+                    ]
+            ])->getBody()->getContents();
+            
+            $jsonObj = json_decode($response);
+
+            return redirect()
+                ->route('category')
+                ->with('success', 'Category has been created!');
+        } catch(\GuzzleHttp\Exception\BadResponseException $e) {
+            if($e->getResponse()->getStatusCode() == 401) {
+                return redirect()
+                    ->route('login');
+            } else {
+                echo($e->getResponse()->getBody());
+            }
+        }
+    }
+
+    public function edit($id)
+    {
+        $jsonObjs = json_decode($this->getCategory($id));
+
+        return view('data.createOrUpdate-category', ['category' => $jsonObjs]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            $token = Session::get('token');
+            $response = $this->client
+                ->request('PUT', $this->base_url.'/category/'.$id, [
+                    'form_params' => [
+                        'category_name' => $request->input('category_name'),    
+                    ],
+                    'headers' => [
+                        'Authorization' => "Bearer {$token}"
+                    ]
+            ])->getBody()->getContents();
+        
+            $jsonObj = json_decode($response);
+
+            return redirect()
+                ->route('category')
+                ->with('success', 'Category has been updated!');
+        } catch(\GuzzleHttp\Exception\BadResponseException $e) {
+            if($e->getResponse()->getStatusCode() == 401) {
+                return redirect()
+                    ->route('login');
+            } else {
+                echo($e->getResponse()->getBody());
+            }
+        }
+    }
 }
